11cd2f000b43ba0d5af703470322499a743b3757
3lue7hunder <tyrusnell@gmail.com>
2025-05-21 23:05:09 -0600

    Week 3

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -1,44 +1,29 @@
--- Insert a new user account into the account table
-INSERT INTO account (account_firstname, account_lastname, account_email, account_password)
+-- 1. Insert
+INSERT INTO public.account (account_firstname, account_lastname, account_email, account_password)
 VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
 
-
-
-
--- Update Tony Stark's account to make him an Admin
-UPDATE account
+-- 2. Modify account type
+UPDATE public.account
 SET account_type = 'Admin'
-WHERE account_email = 'tony@starkent.com';
+WHERE account_firstname = 'Tony' AND account_lastname = 'Stark';
 
+-- 3. Delete from DB
+DELETE FROM public.account
+WHERE account_firstname = 'Tony' AND account_lastname = 'Stark';
 
-
-
--- Delete the Tony Stark account
-DELETE FROM account
-WHERE account_email = 'tony@starkent.com';
-
-
-
-
--- Replace 'small interiors' with 'a huge interior' for Hummer models
-UPDATE inventory
+-- 4. Modify GM Hummer record
+UPDATE public.inventory
 SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
 WHERE inv_make = 'GM' AND inv_model = 'Hummer';
 
-
-
-
--- Show make, model, and classification name for Sport vehicles
+-- 5. Inner Join
 SELECT inv_make, inv_model, classification_name
-FROM inventory
-INNER JOIN classification
-ON inventory.classification_id = classification.classification_id
+FROM public.inventory
+JOIN public.classification
+	ON public.inventory.classification_id = public.classification.classification_id
 WHERE classification_name = 'Sport';
 
-
-
-
--- Update all image paths to include /vehicles/
-UPDATE inventory
-SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
-    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
+-- 6. Update image file paths
+UPDATE public.inventory
+SET inv_image = REPLACE(inv_image, '/images', 'images/vehicles'),
+	inv_thumbnail = REPLACE (inv_thumbnail, '/images', '/images/vehicles');
--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute.js")
+const utilities = require("./utilities/")
+
 
 /* ***********************
  * View Engine and Templates
@@ -17,15 +21,35 @@ const static = require("./routes/static")
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
-
 /* ***********************
  * Routes
  *************************/
 app.use(static)
 
 // Index route
-app.get("/", function(reg, res){
-  res.render("index", {title: "Home"})  
+app.get("/", utilities.handleErrors(baseController.buildHome))
+
+// Inventory routes
+app.use("/inv", inventoryRoute)
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
 })
 
 /* ***********************
@@ -41,6 +65,3 @@ const host = process.env.HOST
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
-
-app.use(express.static("public"))
-
