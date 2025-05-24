a46a4a5072e43a23281829250f0254e80e01928b
3lue7hunder <tyrusnell@gmail.com>
2025-05-24 00:07:52 -0600

    week 3 assignement

--- server.js
+++ server.js
@@ -10,53 +10,36 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-const inventoryRoute = require("./routes/inventoryRoute")
 const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute.js")
 const utilities = require("./utilities/")
+const intentionalErrorRoute = require("./routes/intentionalErrorRoute.js")
 
 
 /* ***********************
  * View Engine and Templates
  *************************/
-
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
-
-
 /* ***********************
  * Routes
  *************************/
-
-// Index Route
-
 app.use(static)
 
+// Index route
 app.get("/", utilities.handleErrors(baseController.buildHome))
 
 // Inventory routes
 app.use("/inv", inventoryRoute)
 
-
-/* ***********************
- * Local Server Information
- * Values from .env (environment) file
- *************************/
-const port = process.env.PORT || 3000
-const host = process.env.HOST
-
-/* ***********************
- * Log statement to confirm server operation
- *************************/
-app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+// Intentional Error route
+app.use("/error", intentionalErrorRoute);
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
-  next({ status: 404, message: 'Sorry, we appear to have lost that page.' })
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
 })
-
 
 /* ***********************
 * Express Error Handler
@@ -71,4 +54,18 @@ app.use(async (err, req, res, next) => {
     message,
     nav
   })
+})
+
+/* ***********************
+ * Local Server Information
+ * Values from .env (environment) file
+ *************************/
+const port = process.env.PORT
+const host = process.env.HOST
+
+/* ***********************
+ * Log statement to confirm server operation
+ *************************/
+app.listen(port, () => {
+  console.log(`app listening on ${host}:${port}`)
 })
