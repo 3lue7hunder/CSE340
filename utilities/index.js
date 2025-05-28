a9ceda734734bf1a95bd688b0467d2da75b7f462
3lue7hunder <tyrusnell@gmail.com>
2025-05-27 23:19:01 -0600

    fix

--- utilities/index.js
+++ utilities/index.js
@@ -9,7 +9,7 @@ Util.getNav = async function (req, res, next) {
   let list = "<ul>"
   console.log(data)
   list += '<li><a href="/" title="Home page">Home</a></li>'
-  data.rows.forEach((row) => {
+  data.forEach((row) => {
     list += "<li>"
     list +=
       '<a href="/inv/type/' +
@@ -36,7 +36,7 @@ Util.buildClassificationGrid = async function(data){
       grid += '<li>'
       grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
       + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
-      + 'details"><img src="' + vehicle.inv_thumbnail 
+      + ' details"><img src="' + vehicle.inv_thumbnail 
       +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
       +' on CSE Motors" /></a>'
       grid += '<div class="namePrice">'
